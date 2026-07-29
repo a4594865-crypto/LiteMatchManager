cd57423b9ef49097daac6e53e467872a8d9c06b9
a4594865-crypto <a4594865@gmail.com>
2026-07-30 05:41:00 +0800

    Update LiteMatchManager.cs

--- LiteMatchManager.cs
+++ LiteMatchManager.cs
@@ -7,6 +7,7 @@ using CounterStrikeSharp.API.Modules.Utils;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Cvars;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using System;
 using System.Linq;
 
@@ -14,14 +15,78 @@ namespace LiteMatchManager;
 
 #pragma warning disable CS8618
 
-public partial class LiteMatchManager : BasePlugin, IPluginConfig<LiteConfig>
+public class LiteMatchConfig : BasePluginConfig
+{
+    [JsonPropertyName("MinPlayersToStart")] public int MinPlayersToStart { get; set; } = 4;
+    [JsonPropertyName("MaxPlayersPerTeam")] public int MaxPlayersPerTeam { get; set; } = 3; 
+    [JsonPropertyName("KickUnreadyPlayerTime")] public int KickUnreadyPlayerTime { get; set; } = 360;
+    
+    [JsonPropertyName("UnreadyReminderInterval")] public int UnreadyReminderInterval { get; set; } = 60;
+    [JsonPropertyName("PublicUnreadyReminderInterval")] public int PublicUnreadyReminderInterval { get; set; } = 15;
+    
+    [JsonPropertyName("WaitingForOpponentInterval")] public int WaitingForOpponentInterval { get; set; } = 30;
+
+    [JsonPropertyName("ChatPrefix")] public string ChatPrefix { get; set; } = "[ {Green}狙 擊 模 式{White} ]";
+    [JsonPropertyName("EnableChatWeaponCommands")] public bool EnableChatWeaponCommands { get; set; } = true;
+    
+    [JsonPropertyName("SpawnWeapons")] 
+    public List<string> SpawnWeapons { get; set; } = ["weapon_knife", "item_assaultsuit", "weapon_awp"];
+    
+    [JsonPropertyName("WarmupConfigName")] public string WarmupConfigName { get; set; } = "warmup.cfg";
+    [JsonPropertyName("LiveConfigName")] public string LiveConfigName { get; set; } = "live.cfg";
+    [JsonPropertyName("Duel_MapChangeDelay")] public int MapChangeDelay { get; set; } = 5;
+    
+    [JsonPropertyName("MapList")] 
+    public List<string> MapList { get; set; } = ["Aim_redline_vieforit:3290337428", "aimpro_vieforit:3290753343"];
+
+    // 各個 HUD 的專屬顯示秒數設定
+    [JsonPropertyName("HudDuration_Prep")] public int HudDuration_Prep { get; set; } = 15;
+    [JsonPropertyName("HudDuration_MatchAbort")] public int HudDuration_MatchAbort { get; set; } = 5;
+    [JsonPropertyName("HudDuration_Round1")] public int HudDuration_Round1 { get; set; } = 8;
+    
+    // 共用的秒數倒數排版
+    [JsonPropertyName("HudHtml_Countdown")] 
+    public string HudHtml_Countdown { get; set; } = "<font class='fontSize-m' color='gray'>視窗關閉倒數：{0} 秒</font>";
+
+    [JsonPropertyName("HudHtml_Prep1v1_Line1")] 
+    public string HudHtml_Prep1v1_Line1 { get; set; } = "<font class='fontSize-l' color='white'>✦ 觸 發 1 v 1 單 挑 ✦</font>";
+    
+    [JsonPropertyName("HudHtml_Prep1v1_Line2")] 
+    public string HudHtml_Prep1v1_Line2 { get; set; } = "<font class='fontSize-l' color='gray'>進度： </font><font class='fontSize-l' color='lime'>{0} / 2</font><font class='fontSize-l' color='gray'> ( 尚缺 {1} 人 )</font>";
+    
+    [JsonPropertyName("HudHtml_Prep2v2_Line1")] 
+    public string HudHtml_Prep2v2_Line1 { get; set; } = "<font class='fontSize-l' color='white'>✦ 觸 發 2 v 2 團 戰 ✦</font>";
+    
+    [JsonPropertyName("HudHtml_Prep2v2_Line2")] 
+    public string HudHtml_Prep2v2_Line2 { get; set; } = "<font class='fontSize-l' color='gray'>進度： </font><font class='fontSize-l' color='lime'>{0} / {2}</font><font class='fontSize-l' color='gray'> ( 尚缺 {1} 人 )</font>";
+
+    [JsonPropertyName("HudHtml_Prep3v3_Line1")] 
+    public string HudHtml_Prep3v3_Line1 { get; set; } = "<font class='fontSize-l' color='white'>✦ 觸 發 3 v 3 大 亂 鬥 ✦</font>";
+    
+    [JsonPropertyName("HudHtml_Prep3v3_Line2")] 
+    public string HudHtml_Prep3v3_Line2 { get; set; } = "<font class='fontSize-l' color='gray'>進度： </font><font class='fontSize-l' color='lime'>{0} / {2}</font><font class='fontSize-l' color='gray'> ( 尚缺 {1} 人 )</font>";
+    
+    [JsonPropertyName("HudHtml_MatchAbort_Line1")] 
+    public string HudHtml_MatchAbort_Line1 { get; set; } = "<font class='fontSize-l' color='red'>[ 警 告 ] 玩 家 逃 跑 ， 戰 鬥 終 止</font>";
+
+    [JsonPropertyName("HudHtml_MatchAbort_Line2")] 
+    public string HudHtml_MatchAbort_Line2 { get; set; } = "<font class='fontSize-l' color='white'>已 退 回 暖 身 模 式</font>";
+
+    [JsonPropertyName("HudHtml_Round1_Line1")] 
+    public string HudHtml_Round1_Line1 { get; set; } = "<font class='fontSize-l' color='gold'>✦ 戰 鬥 開 始 ✦</font>";
+
+    [JsonPropertyName("HudHtml_Round1_Line2")] 
+    public string HudHtml_Round1_Line2 { get; set; } = "<font class='fontSize-l' color='white'>率 先 取 得 </font><font class='fontSize-xxl' color='lime'><b>２０</b></font><font class='fontSize-xxl' color='white'> 勝 者 為 贏 家</font>";
+}
+
+public partial class LiteMatchManager : BasePlugin, IPluginConfig<LiteMatchConfig>
 {
     public override string ModuleName => "LiteMatchManager";
-    public override string ModuleVersion => "8.54_Config_Rename";
+    public override string ModuleVersion => "8.54_HUD_Split";
     public override string ModuleAuthor => "Optimized";
-    public override string ModuleDescription => "主程式與 HUD、Config 拆分版";
+    public override string ModuleDescription => "主程式與 HUD 拆分模組化版本";
 
-    public LiteConfig Config { get; set; } = new LiteConfig();
+    public LiteMatchConfig Config { get; set; } = new LiteMatchConfig();
 
     private string _cachedPrefix = "";
     private HashSet<ulong> _readyPlayers = new(64);
@@ -55,7 +120,7 @@ public partial class LiteMatchManager : BasePlugin, IPluginConfig<LiteConfig>
 
     private void OnTick()
     {
-        // 呼叫 LiteHud.cs 裡的 HUD 渲染與倒數邏輯
+        // 呼叫另一個檔案 (LiteMatchManager.Hud.cs) 裡的 HUD 渲染與倒數邏輯
         HandleHudTick();
 
         if (!_gameRulesInitialized) InitializeGameRules();
@@ -106,7 +171,7 @@ public partial class LiteMatchManager : BasePlugin, IPluginConfig<LiteConfig>
         }
     }
 
-    public void OnConfigParsed(LiteConfig config)
+    public void OnConfigParsed(LiteMatchConfig config)
     {
         Config = config;
         _cachedPrefix = config.ChatPrefix
@@ -123,7 +188,7 @@ public partial class LiteMatchManager : BasePlugin, IPluginConfig<LiteConfig>
     public override void Load(bool hotReload)
     {
         Console.WriteLine("=================================================");
-        Console.WriteLine("  LiteMatchManager v8.54 (檔案拆分版) 啟動！");
+        Console.WriteLine("  LiteMatchManager v8.54 (拆分模組化版) 啟動！");
         Console.WriteLine("=================================================");
 
         _isServerShuttingDown = false;
@@ -194,7 +259,7 @@ public partial class LiteMatchManager : BasePlugin, IPluginConfig<LiteConfig>
                     {
                         _readyPlayers.Remove(steamId);
                         if (!_isMatchLive)
-                            Server.PrintToChatAll($" {_cachedPrefix} {ChatColors.Orange}{player.PlayerName}{ChatColors.White} 跳 去 觀 戰，已 取 configuration 消 準 備");
+                            Server.PrintToChatAll($" {_cachedPrefix} {ChatColors.Orange}{player.PlayerName}{ChatColors.White} 跳 去 觀 戰，已 取 消 準 備");
                         else
                             Server.PrintToChatAll($" {_cachedPrefix} {ChatColors.Orange}{player.PlayerName}{ChatColors.White} 退 出 了 戰 鬥，移 至 觀 戰 ");
                     }
